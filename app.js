695e3d353d64484bf87211d556e3f59797e1d652
akshaynarwadkar <90238392+akshaynarwadkar@users.noreply.github.com>
2021-11-09 19:06:17 +0530

    doubt on html tags

--- app.js
+++ app.js
@@ -1,36 +1,60 @@
 
 const form = document.querySelector('form.add')
-const ul = document.querySelector('.list-group')
+const search = document.querySelector('form.search')
+const ul = document.querySelectorAll('ul li')
 
 // console.log(form)
-// console.log(ul)
+//  console.log(ul)
+console.log(search)
 
-form.addEventListener('submit', e =>{
-    e.preventDefault()
-    const todo = form.add.value.trim()
+// form.addEventListener('submit', e =>{
+//     e.preventDefault()
+//     const todo = form.add.value.trim()
 
-    if(todo.length)
-    {
-        const html = `
-        <li class="list-group-item d-flex justify-content-between align-items-center">
-            <span>${todo}</span>
-            <i class="far fa-trash-alt delete"></i>
-        </li>`
+//     if(todo.length)
+//     {
+//         const html = `
+//         <li class="list-group-item d-flex justify-content-between align-items-center">
+//             <span>${todo}</span>
+//             <i class="far fa-trash-alt delete"></i>
+//         </li>`
 
-        ul.innerHTML += html
+//         ul.innerHTML += html
         
-    }   
-
-    form.reset() //we reset the form not just the value 
+//     }   
 
 
-})
+//     form.reset() //we reset the form not just the value 
 
-ul.addEventListener('click',e => {
-    if(e.target.nodeName === 'I'){
+
+// })
+
+// ul.addEventListener('click',e => {
+//     if(e.target.nodeName === 'I'){
         
-        e.target.parentElement.remove()
+//         e.target.parentElement.remove()
         
         
-    }
-})
+//     }
+// })
+
+
+    
+    
+//     search.addEventListener('keyup' ,() =>{
+//         ul.forEach(item=>{
+//          if(search.search.value === item){
+//             const html = `
+//                 <li class="list-group-item d-flex justify-content-between align-items-center">
+//                     <span>${item}</span>
+//                 <i class="far fa-trash-alt delete"></i>
+//                 </li>`
+//                 console.log(html)
+//     }
+
+// })
+// })
+
+
+
+
